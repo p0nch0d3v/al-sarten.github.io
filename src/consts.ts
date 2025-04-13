fcdec1fa6a83b585134ca550e7deadb5b8cf75bb
Alfonso Gonzalez <alfonso.gonzalez@3pillarglobal.com>
2025-04-13 16:39:03 -0700

    initial comit,

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const SITE = {
-  title: 'Documentation',
+  title: 'Al Sarten',
   description: 'Your website description.',
-  defaultLanguage: 'en-us'
+  defaultLanguage: 'es-mx'
 } as const
 
 export const OPEN_GRAPH = {
@@ -15,8 +15,7 @@ export const OPEN_GRAPH = {
 }
 
 export const KNOWN_LANGUAGES = {
-  Deutsch: 'de',
-  English: 'en'
+  Espanol: 'es'
 } as const
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES)
 
@@ -36,20 +35,9 @@ export type Sidebar = Record<
   Record<string, { text: string; link: string }[]>
 >
 export const SIDEBAR: Sidebar = {
-  de: {
-    Überschrift: [
-      { text: 'Einführung', link: 'de/introduction' },
-      { text: 'Seite 2', link: 'de/page-2' },
-      { text: 'Seite 3', link: 'de/page-3' }
-    ],
-    'Ein weiterer Abschnitt': [{ text: 'Seite 4', link: 'de/page-4' }]
-  },
-  en: {
-    'Section Header': [
-      { text: 'Introduction', link: 'en/introduction' },
-      { text: 'Page 2', link: 'en/page-2' },
-      { text: 'Page 3', link: 'en/page-3' }
-    ],
-    'Another Section': [{ text: 'Page 4', link: 'en/page-4' }]
+  es: {
+    'Algoritmos': [
+      { text: 'Al Sarten', link: 'buen-provecho' }
+    ]
   }
 }
